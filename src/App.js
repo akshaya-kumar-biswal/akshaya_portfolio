6e683954225e78fd456075ea55000066f887c9b8
akshaya-kumar-biswal <mail.akshayabiswal@gmail.com>
2021-06-29 08:56:13 +0530

    NavMenu and Basic Pages Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import NavMenu from './components/NavManu';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Home from './pages/Home';
+import Projects from './pages/Projects';
 
-export default function App() {
-  return (
-    <>
-      <h1>Welcome to react portfolio Project</h1>
-      <h3>-Shaif Arfan</h3>
-    </>
-  );
-}
+const App = () => (
+  <Router>
+    <NavMenu />
+    <Switch>
+      <Route path="/about" component={About} />
+      <Route path="/projects" component={Projects} />
+      <Route path="/contact" component={Contact} />
+      <Route path="/" component={Home} />
+    </Switch>
+  </Router>
+);
+
+export default App;
