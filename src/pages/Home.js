0f656a93c4e6de2a3549d4d677c0f3873c329c4b
akshaya-kumar-biswal <mail.akshayabiswal@gmail.com>
2021-06-30 23:25:45 +0530

    Service Section Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import HomeSection from '../components/HomeSection';
 import AboutSection from '../components/AboutSection';
+import ServicesSection from '../components/ServicesSection';
 
 const Home = () => (
   <>
     <HomeSection />
     <AboutSection />
+    <ServicesSection />
   </>
 );
 
