e0f724df9524f1b5b49b53163a22c22e82f5ac38
akshaya-kumar-biswal <mail.akshayabiswal@gmail.com>
2021-07-01 07:45:54 +0530

    Project Section Added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,12 +2,14 @@ import React from 'react';
 import HomeSection from '../components/HomeSection';
 import AboutSection from '../components/AboutSection';
 import ServicesSection from '../components/ServicesSection';
+import ProjectsSection from '../components/ProjectsSection';
 
 const Home = () => (
   <>
     <HomeSection />
     <AboutSection />
     <ServicesSection />
+    <ProjectsSection />
   </>
 );
 
